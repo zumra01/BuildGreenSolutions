206e3e364c3d4c2ab57dc86d97d70b6770835182
zumra01 <zumraqamar@gmail.com>
2026-01-25 20:28:17 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// You can add custom JS here if needed.
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you for contacting us! We will get back to you soon.");
+});
